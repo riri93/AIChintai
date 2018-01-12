660f19ddee36ca33156ba3b35c740d389348dd86
rihabkallel <rkallel@hrdatabank.com>
2018-01-12 16:52:48 +0100

    sss

--- src/main/java/com/example/repository/StationRepository.java
+++ src/main/java/com/example/repository/StationRepository.java
@@ -14,7 +14,7 @@ import com.example.entity.Station;
 @Repository
 
 public interface StationRepository extends JpaRepository<Station, Serializable> {
-	@Query(value = "SELECT s.* from  Station s where lower(s.stationName) like lower(CONCAT('%',:station,'%')) or abs(cast( GREATEST(length(:station),length(n.japanese_station)) -levenshtein( lower(:station),lower(s.stationName)) as float))/length(:station)*100 >40  desc ,    ?#{#pageable}", nativeQuery = true, countQuery = "SELECT count(s.*) from  station s where lower(s.stationName) like lower(CONCAT('%',:station,'%')) or abs(cast( GREATEST(length(:station),length(n.japanese_station)) -levenshtein( lower(:station),lower(s.stationName)) as float))/length(:station)*100 >40  desc ,  ?#{#pageable}  ")
+	@Query(value = "SELECT s.* from  Station s where lower(s.stationName) like lower(CONCAT('%',:station,'%')) or abs(cast( GREATEST(length(:station),length(s.stationName)) -levenshtein( lower(:station),lower(s.stationName)) as float))/length(:station)*100 >40  desc ,    ?#{#pageable}", nativeQuery = true, countQuery = "SELECT count(s.*) from  station s where lower(s.stationName) like lower(CONCAT('%',:station,'%')) or abs(cast( GREATEST(length(:station),length(s.stationName)) -levenshtein( lower(:station),lower(s.stationName)) as float))/length(:station)*100 >40  desc ,  ?#{#pageable}  ")
 	public Page findStations(@Param("station") String station, Pageable pageable);
 
 	@Query("SELECT a FROM Station a where a.idStation = :id")
