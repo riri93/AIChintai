ef5312a53b501d6ac5c5401655cb1b7501e63cc0
rihabkallel <rkallel@hrdatabank.com>
2018-01-16 16:15:25 +0100

    url

--- src/main/java/com/example/controller/ChintaiBotController.java
+++ src/main/java/com/example/controller/ChintaiBotController.java
@@ -458,7 +458,7 @@ public class ChintaiBotController {
 
 			String title = "";
 
-			String img = "http://danvillearearentals.com/wp-content/uploads/2014/11/House-for-Rent.jpg";
+			String img = "https://cdn2.iconfinder.com/data/icons/employment-business/256/Job_Search-512.png";
 
 			title = room.getNameBuilding();
 
