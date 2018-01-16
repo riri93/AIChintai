54cb77e30e14d7c13e1aba84ebc4b8c7c7ce7902
rihabkallel <rkallel@hrdatabank.com>
2018-01-16 15:12:27 +0100

    query

--- src/main/java/com/example/repository/RoomRepository.java
+++ src/main/java/com/example/repository/RoomRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
@@ -17,7 +18,7 @@ public interface RoomRepository extends JpaRepository<Room, Serializable> {
 	@Query(value = "SELECT DISTINCT(r) FROM Room r")
 	public List<Room> findRoomsByAllFields();
 
-	@Query(value = "SELECT DISTINCT(r) FROM Room r where r.price>=minPrice and r.price<=maxPrice")
-	public List<Room> findRoomsByPrice(int minPrice, int maxPrice);
+	@Query(value = "SELECT DISTINCT(r) FROM Room r where r.price>=:minPrice and r.price<=:maxPrice")
+	public List<Room> findRoomsByPrice(@Param("minPrice") int minPrice, @Param("maxPrice") int maxPrice);
 
 }
