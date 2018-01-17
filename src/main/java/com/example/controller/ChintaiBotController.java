54c13279bc85b2c484b557115bf5684986ad4629
rihabkallel <rkallel@hrdatabank.com>
2018-01-17 12:41:30 +0100

    unused import

--- src/main/java/com/example/controller/ChintaiBotController.java
+++ src/main/java/com/example/controller/ChintaiBotController.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import org.json.JSONException;
